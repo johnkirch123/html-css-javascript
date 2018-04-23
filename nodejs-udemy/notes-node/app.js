c7ac9f526d6e4a5c673b8d02112a31812aeca852
John Kirch <john.kirch.business@gmail.com>
2018-04-22 20:03:44 -0600

    requiring and json
    parsing string to json and stringify, requiring modules

--- nodejs-udemy/notes-node/app.js
+++ nodejs-udemy/notes-node/app.js
@@ -1,18 +1,24 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ =  require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-console.log('Result: ', notes.add(9, -2));
-// var result = notes.add(5, 9);
-// console.log(result);
-// var res = notes.addNote();
-// console.log(res);
-// var user = os.userInfo();
-//
-// fs.appendFile('grettings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
-//   if (err) {
-//     console.log('Unable to write to file');
-//   }
-// });
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ', command);
+console.log('Yargs: ', argv);
+
+if (command === 'add') {
+  notes.addNote(argv.title, argv.body);
+} else if (command === 'list') {
+  notes.getAll();
+} else if (command === 'read') {
+  notes.getNote(argv.title);
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+} else {
+  console.log('Command not recognized');
+}

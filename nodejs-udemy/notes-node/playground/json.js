f3db419cef77cc47b9b1d99ed4c62e56a137e71e
John Kirch <john.kirch.business@gmail.com>
2018-05-05 18:19:56 -0600

    nodejs
    node js note builider

--- nodejs-udemy/notes-node/playground/json.js
+++ nodejs-udemy/notes-node/playground/json.js
@@ -5,7 +5,22 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-var personString = '{"name": "John", "age": 37}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// var personString = '{"name": "John", "age": 37}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+const fs = require('fs');
+
+var originalNote = {
+  title: 'Some Title',
+  body: 'Some Body'
+};
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+console.log(typeof note);
+console.log(note.title);

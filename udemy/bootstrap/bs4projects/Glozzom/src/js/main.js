4e2d6779c08ad51d82f6337cd9df089f932e4173
John Kirch <john.kirch.business@gmail.com>
2018-06-02 21:34:20 -0600

    Completed Gozzom project
    Gozzom Bootstrap project from Udemy Traversy Media tutorial.

---
+++ udemy/bootstrap/bs4projects/Glozzom/src/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+  // Autoplay modal video
+  $(".video").click(function() {
+    var theModal = $(this).data("target"),
+      videoSRC = $(this).attr("data-video"),
+      videoSRCauto =
+        videoSRC +
+        "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+    $(theModal + " iframe").attr("src", videoSRCauto);
+    $(theModal + " button.close").click(function() {
+      $(theModal + " iframe").attr("src", videoSRC);
+    });
+  });
+});
+
+$(document).on("click", '[data-toggle="lightbox"]', function() {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
+$(".slider").slick({
+  infinite: true,
+  slideToShow: 1,
+  slideToScroll: 1
+});
+
+// $(".slider").slick({
+//   infinite: true,
+//   slideToShow: 1,
+//   slideToScroll: 1,
+//   autoplay: true,
+//   autoplayspeed: 2000
+// });

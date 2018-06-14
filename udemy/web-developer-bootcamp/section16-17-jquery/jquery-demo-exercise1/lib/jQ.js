57fdc1a9f76de15e17a7a0b2b769b52026f76fe9
johnkirch123 <john.kirch.business@gmail.com>
2018-06-14 00:41:18 -0600

    jQuery WDB
    Completed Intro to jQuery, started advanced jQuery.

--- udemy/web-developer-bootcamp/section16-17-jquery/jquery-demo-exercise1/lib/jQ.js
+++ udemy/web-developer-bootcamp/section16-17-jquery/jquery-demo-exercise1/lib/jQ.js
@@ -1,4 +1,4 @@
 $("div").css("backgroundColor", "purple");
 $(".highlight").css("width", "200px");
 $("#third").css("border", "1px solid orange");
-$("div:nth(0)").css("color", "pink");
+$("div:first-of-type").css("color", "pink");

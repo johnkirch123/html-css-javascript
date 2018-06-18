4f3edb504f7c4476f7a4b45453c939360985e928
johnkirch123 <john.kirch.business@gmail.com>
2018-06-17 23:58:57 -0600

    methods and todos
    WDB Udemy

---
+++ udemy/web-developer-bootcamp/section16-17-jquery/todo-list/assets/js/todos.js
@@ -0,0 +1,26 @@
+// check off specific to-dos by clicking
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("completed");
+});
+
+// click on x to delete to-do
+$("ul").on("click", "span", function(e) {
+  $(this)
+    .parent()
+    .fadeOut(500, function() {
+      $(this).remove();
+    });
+  e.stopPropagation();
+});
+
+$("input[type='text']").on("keypress", function(e) {
+  if (e.which === 13) {
+    // grabbing new todo text from input
+    var todoText = $(this).val();
+    $(this).val("");
+    // create a new li and add to ul
+    $("ul").append(
+      "<li><span><i class='fa fa-trash-alt'></i></span> " + todoText + "</li>"
+    );
+  }
+});

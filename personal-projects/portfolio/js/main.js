465f3796270df3bf001641aacb728549849a3f83
John Kirch <john.kirch.business@gmail.com>
2018-07-17 00:30:18 -0600

    javascript, started getting nav to work

--- personal-projects/portfolio/js/main.js
+++ personal-projects/portfolio/js/main.js
@@ -1,13 +1,20 @@
-$(function() {
-  var header = $(".header");
-  $(window).scroll(function() {    
-      var scroll = $(window).scrollTop();
-  
-      if (scroll >= 100) {
-        document.body.setAttribute("background-color", "red");
-        document.getElementById("nav__button").setAttribute("display", "block");
-      } else {
-        document.getElementById("nav__button").setAttribute("display", "block");
-      }
-  });
-});
+
+
+    /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+  window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  var btn = document.getElementById("nav__btn");
+  var bg = document.getElementById("nav__bg");
+  if (currentScrollPos > 0) {
+    btn.classList.add("hidden");
+    bg.classList.add("hidden");
+  } else {
+    btn.classList.remove("hidden");
+    bg.classList.remove("hidden");
+  }
+  prevScrollPos = currentScrollPos;
+}
+
+function uncheck() {
+  document.querySelector("#navi-toggle").checked = false;
+}

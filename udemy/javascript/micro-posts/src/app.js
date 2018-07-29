d301fbd57376a91bf58602be9e2c763db2abe6e3
John Kirch <john.kirch.business@gmail.com>
2018-07-29 14:20:21 -0600

    completed javascript course and micro posts api

--- udemy/javascript/micro-posts/src/app.js
+++ udemy/javascript/micro-posts/src/app.js
@@ -5,7 +5,16 @@ import { ui } from "./ui";
 document.addEventListener("DOMContentLoaded", getPosts);
 
 // Listen for add post
-document.querySelector(".post-submit").addEventListener("click", addPost);
+document.querySelector(".post-submit").addEventListener("click", submitPost);
+
+// Listen for delete post
+document.querySelector('#posts').addEventListener('click', deletePost);
+
+// Listen for edit state
+document.querySelector('#posts').addEventListener('click', enableEdit);
+
+// Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
 
 // Get Posts
 function getPosts() {
@@ -19,17 +28,86 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector("#title").value;
   const body = document.querySelector("#body").value;
-
+  const id = document.querySelector("#id").value;
+  
   const data = {
     title,
     body
   };
 
-  // Create Post
-  http
-    .post("http://localhost:3000/posts", data)
-    .then(data => {
-      getPosts();
-    })
-    .catch(err => console.log(err));
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    // Check for ID
+    if(id === '') { 
+        // Create Post
+      http
+      .post("http://localhost:3000/posts", data)
+      .then(data => {
+        ui.showAlert('Post added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+      })
+      .catch(err => console.log(err));
+    } else {
+      // Update post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+          .then(data => {
+            ui.showAlert('Post updated', 'alert alert-success');
+            ui.changeFormState('.add');
+            getPosts();
+          })
+          .catch(err => console.log(err));
+    }
+
+  
+    
+  }
+
+}
+
+// Delete post
+function deletePost(e) {
+  if(e.target.parentElement.classList.contains('delete')) {
+    const id = e.target.parentElement.dataset.id;
+    if(confirm('Are you sure?')) {
+      http.delete(`http://localhost:3000/posts/${id}`)
+          .then(data => {
+            ui.showAlert('Post removed', 'alert alert-success');
+            getPosts();
+          })
+          .catch(err => console.log(err));
+    }
+  }
+
+  e.preventDefault();
+}
+
+// Enable Edit state
+function enableEdit(e) {
+  if(e.target.parentElement.classList.contains('edit')) {
+    const id = e.target.parentElement.dataset.id;
+    const title = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+    const body = e.target.parentElement.previousElementSibling.textContent;
+
+    const data = {
+      id,
+      title,
+      body
+    }
+
+    // Fill the form with current post
+    ui.fillForm(data);
+  }
+
+  e.preventDefault();
+}
+
+// Cancel edit state
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
+  }
+
+  e.preventDefault();
 }
--- udemy/javascript/micro-posts/src/ui.js
+++ udemy/javascript/micro-posts/src/ui.js
@@ -8,6 +8,7 @@ class UI {
     this.forState = 'add';
   }
 
+  // show all posts
   showPosts(posts) {
     let output = '';
 
@@ -29,6 +30,88 @@ class UI {
     });
 
     this.post.innerHTML = output;
+  }
+
+  // show alert message
+  showAlert(message, className) {
+    this.clearAlert();
+
+    // Create div
+    const div = document.createElement('div');
+    // Add classes
+    div.className = className;
+    // Add text
+    div.appendChild(document.createTextNode(message));
+    // Get parent
+    const container = document.querySelector('.postsContainer');
+    // Get posts
+    const posts = document.querySelector('#posts');
+    // Insert alert div
+    container.insertBefore(div, posts);
+
+    // Timeout
+    setTimeout(() => {
+      this.clearAlert();
+    }, 3000);
+  }
+
+  // Clear alert notification
+  clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+
+    if(currentAlert) {
+      currentAlert.remove();
+    }
+  }
+
+  // Clear all fields
+  clearFields() {
+    this.titleInput.value = '';
+    this.bodyInput.value = '';
+  }
+
+  // Fill form to edit
+  fillForm(data) {
+    this.titleInput.value = data.title;
+    this.bodyInput.value = data.body;
+    this.idInput.value = data.id;
+
+    this.changeFormState('edit');
+  }
+
+  // Clear ID hidden value
+  clearIdInput() {
+    this.idInput.value = '';
+  }
+
+  // Change the form state
+  changeFormState(type) {
+    if(type === 'edit') {
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // Create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+      // Get parent
+      const cardForm = document.querySelector('.card-form');
+      // Get element to insert before
+      const formEnd = document.querySelector('.form-end');
+      // Insert the cancel button
+      cardForm.insertBefore(button, formEnd);
+    } else {
+      this.postSubmit.textContent = 'Post It';
+      this.postSubmit.className = 'post-submit btn btn-primary btn-block';
+      // Remove cancel btn if it is there
+      if(document.querySelector('.post-cancel')) {
+        document.querySelector('.post-cancel').remove();
+      }
+      // Clear ID from hidden field
+      this.clearIdInput();
+      // Clear text
+      this.clearFields();
+    }
   }
 }
 

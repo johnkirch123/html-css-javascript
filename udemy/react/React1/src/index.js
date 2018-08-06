8d2723b78dc59aeb0fa99a9be3c587f7965b1c58
John Kirch <john.kirch.business@gmail.com>
2018-08-05 19:11:04 -0600

    youtube api with react

--- udemy/react/React1/src/index.js
+++ udemy/react/React1/src/index.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-const API_KEY ='AIzaSyDMcC-sT-yz984tPbH7W-89bqrwsN-OKk0';
+const API_KEY ='AIzaSyCRP8FwIXFXLxzYXsqKqNq9_9jwYCl8NFI';
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  ); 
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [],
+                   selectedVideo: null
+                };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+      // console.log(videos);
+      this.setState({ 
+        videos: videos,
+        selectedVideo: videos[0]
+       });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    ); 
+  }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)

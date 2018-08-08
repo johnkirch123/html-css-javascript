6dcc91b84c793b004e6c5abadd4202ea2d326d7e
John Kirch <john.kirch.business@gmail.com>
2018-08-07 22:40:17 -0600

    completed vidly app

--- udemy/react-mosh/vidly/src/index.js
+++ udemy/react-mosh/vidly/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import 'font-awesome/css/font-awesome.css';
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

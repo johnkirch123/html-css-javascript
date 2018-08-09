e6129de918d07b137fe48530472d71d1c34e4b84
John Kirch <john.kirch.business@gmail.com>
2018-08-08 21:45:35 -0600

    changes to counter-app

--- udemy/react-mosh/counter-app/src/components/counter.jsx
+++ udemy/react-mosh/counter-app/src/components/counter.jsx
@@ -3,46 +3,45 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
     tags: ['tag1', 'tag2', 'tag3']
   };
 
   handleIncrement = product => {
-    console.log(product);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   }
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  // renderTags() {
+  //   if (this.state.tags.length === 0) return <p>There are no tags!</p>;
 
-    return (
-      <ul>
-        { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
-      </ul>
-    );
-  }
+  //   return (
+  //     <ul>
+  //       { this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
+  //     </ul>
+  //   );
+  // }
 
   render() { 
     return ( 
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={() => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
-        { this.state.tags.length === 0 && 'Please create a new tag!'}
-        { this.renderTags() }
+        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        {/* { this.state.tags.length === 0 && 'Please create a new tag!'}
+        { this.renderTags() } */}
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
 
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    const { value } = this.state;
+    return value === 0 ? 'Zero' : value;
   }
 }
  

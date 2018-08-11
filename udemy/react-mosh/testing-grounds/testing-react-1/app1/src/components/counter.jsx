fa3ce4607d0a77d3e3083fe4e1a3e443b563509e
johnkirch123 <john.kirch.business@gmail.com>
2018-08-11 00:43:59 -0600

    made a few more edits to my counter app

--- udemy/react-mosh/testing-grounds/testing-react-1/app1/src/components/counter.jsx
+++ udemy/react-mosh/testing-grounds/testing-react-1/app1/src/components/counter.jsx
@@ -1,25 +1,34 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = { count: 0 };
+  state = {
+    count: 1,
+    tags: ["tag1", "tag2", "tag3"]
+  };
   render() {
     return (
       <div className="container bg-light mt-5">
-        <span className="numberAmount">{this.state.count}</span>
-        <button
-          onClick={this.incrementHandler}
-          className="btn btn-outline-primary btn-lg btn-block py-2 mt-4"
-        >
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-outline-primary btn-lg btn-block py-2 mt-4">
           Increment
         </button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
 
-  incrementHandler() {
-    this.setState(() => {
-      this.state.count += this.state.count + 1;
-    });
+  getBadgeClasses() {
+    let classes = "numberAmount badge mb-4 badge-";
+    return (classes += this.state.count === 0 ? "warining" : "primary");
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 

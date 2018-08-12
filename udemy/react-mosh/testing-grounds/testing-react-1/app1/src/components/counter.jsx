c226bebd9aec5753f164cc8366ea009104704a83
John Kirch <john.kirch.business@gmail.com>
2018-08-12 00:01:47 -0600

    worked more on React counter app in testing area

--- udemy/react-mosh/testing-grounds/testing-react-1/app1/src/components/counter.jsx
+++ udemy/react-mosh/testing-grounds/testing-react-1/app1/src/components/counter.jsx
@@ -1,35 +1,55 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 1,
-    tags: ["tag1", "tag2", "tag3"]
-  };
+  // state = {
+  //   value: this.props.counter.value
+  //   // tags: ["tag1", "tag2", "tag3"]
+  // }
+
   render() {
     return (
       <div className="container bg-light mt-5">
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-outline-primary btn-lg btn-block py-2 mt-4">
+        <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-outline-primary btn-lg btn-block py-2 mt-4">
           Increment
         </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button 
+          onClick={() => this.props.onDecrement(this.props.counter)} 
+          className="btn btn-outline-warning btn-lg btn-block py-2 mt-4">
+          Decrement
+        </button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-outline-danger btn-lg btn-block py-2 mt-4">
+          Delete
+        </button>
       </div>
     );
   }
 
+  // handleIncrement = () => {
+  //   this.setState({ value: this.state.value + 1 });
+  // }
+
+  // handleDecrement = () => {
+  //   this.setState({ value: this.state.value - 1 });
+  // }
+
   getBadgeClasses() {
     let classes = "numberAmount badge mb-4 badge-";
-    return (classes += this.state.count === 0 ? "warining" : "primary");
+    return (classes += this.props.counter.value < 0 ? "warning" : "success");
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
+
 }
 
 export default Counter;
+
+
+/*{ <ul>
+  {this.state.tags.map(tag => (
+    <li key={tag}>{tag}</li>
+  ))}
+</ul>} */

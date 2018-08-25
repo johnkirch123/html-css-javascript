7f3788823005775b52ab4616af65c83034283dbb
johnkirch123 <john.kirch.business@gmail.com>
2018-08-25 01:29:47 -0600

    worked on some validation and redux form.

--- udemy/react/post-app/src/components/posts_new.jsx
+++ udemy/react/post-app/src/components/posts_new.jsx
@@ -7,29 +7,62 @@ class PostsNew extends Component {
       <div className="form-group">
         <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
       </div>
     );
   }
 
+  onSubmit(values) {
+    console.log(values);
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           name="title"
           label="Title For Post"
           component={this.renderField}
         />
-        <Field name="tags" label="Tags" component={this.renderField} />
+        <Field
+          name="categories"
+          label="Categories"
+          component={this.renderField}
+        />
         <Field
           name="content"
           label="Post Content"
           component={this.renderField}
         />
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     );
   }
 }
 
+function validate(values) {
+  const errors = {};
+
+  // Validate inputs from values object
+  if (!values.title) {
+    errors.title = "Enter a title!";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter a categories!";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please!";
+  }
+  // If errors is empty, the form is fine to submit
+  // If erros has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
 export default reduxForm({
+  validate,
   form: "PostsNewForm"
 })(PostsNew);

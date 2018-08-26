77751cb9957117c0d007fa2bc57068caeda67ed4
John Kirch <john.kirch.business@gmail.com>
2018-08-26 00:12:33 -0600

    completed posts-app

--- udemy/react/post-app/src/components/posts_show.jsx
+++ udemy/react/post-app/src/components/posts_show.jsx
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchPost } from "../actions/action_index";
+import { Link } from "react-router-dom";
+import { fetchPost, deletePost } from "../actions/action_index";
 
 class PostsShow extends Component {
   componentDidMount() {
-    const { id } = this.props.match.params;
-    this.props.fetchPost(id);
+    if (!this.props.post) {
+      const { id } = this.props.match.params;
+      this.props.fetchPost(id);
+    }
   }
+
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+    this.props.deletePost(id, () => {
+      this.props.history.push("/");
+    });
+  }
+
   render() {
     const { post } = this.props;
 
     if (!post) {
-      return <div>Loading</div>;
+      return <div>Loading...</div>;
     }
 
     return (
       <div>
+        <Link to="/" id="posts-show-back" className="btn btn-primary">
+          Back To Index
+        </Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
@@ -30,5 +50,5 @@ function mapStateToProps({ posts }, ownProps) {
 
 export default connect(
   mapStateToProps,
-  { fetchPost }
+  { fetchPost, deletePost }
 )(PostsShow);
--- udemy/react/post-app/src/reducers/reducer_posts.jsx
+++ udemy/react/post-app/src/reducers/reducer_posts.jsx
@@ -1,8 +1,11 @@
-import { FETCH_POSTS, FETCH_POST } from "../actions/action_index";
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from "../actions/action_index";
 import _ from "lodash";
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload);
+
     case FETCH_POST:
       // const post = action.payload.data;
       // const newState = { ...state };

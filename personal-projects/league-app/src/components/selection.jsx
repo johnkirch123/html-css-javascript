af2d830a954ef935596ab7874d5e2f1ac1e7ecd8
John Kirch <john.kirch.business@gmail.com>
2018-08-27 23:15:37 -0600

    installed express and notes on tying api with react

--- personal-projects/league-app/src/components/selection.jsx
+++ personal-projects/league-app/src/components/selection.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-
+// Will need to use the api on the back end to get data,
+// tie in the api with this file from the backend.
 class Selection extends Component {
   state = {};
   render() {

520b7879ed8d811ed3181873430066485a483a0e
John Kirch <john.kirch.business@gmail.com>
2018-08-31 13:45:11 -0600

    continuing to work on weather api, copied more than I want to.

---
+++ personal-projects/Weather App/src/containers/search_bar.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { fetchWeather } from "../actions/index";
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+
+  onInputChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({ term: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onFormSubmit} className="input-group">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter a city name for 5 day forecast"
+          value={this.state.term}
+          onChange={this.onInputChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">
+            Search
+          </button>
+        </span>
+      </form>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(SearchBar);

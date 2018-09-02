90a863b58de798e3d794e2c9988e09523ac495ad
John Kirch <john.kirch.business@gmail.com>
2018-09-01 20:54:38 -0600

    got the Riot API working, not as much info there as I had hoped.

--- personal-projects/league-app/src/server.js
+++ personal-projects/league-app/src/server.js
@@ -1,13 +1,27 @@
-var express = require("express");
-var app = express();
-var exphbs = require("express-handlebars");
-var request = require("request");
-var async = require("async");
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+const express = require("express"),
+  app = express(),
+  ejs = require("ejs"),
+  request = require("request");
+mongoose = require("mongoose");
 
-app.set("view engine", "handlebars");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get("/", function(req, res) {});
+const API_KEY = "RGAPI-488693fa-4495-4dcc-ba1d-d1b3f8017f35";
+const ACCOUNT_ID = "223438398";
+const ROOT_URL = `https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/dubskiski?api_key=${API_KEY}`;
+const MATCH_URL = `https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${ACCOUNT_ID}?api_key=${API_KEY}`;
 
-var port = Number(process.env.PORT || 3000);
+app.get("/", function(req, res) {
+  request(ROOT_URL, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const data = JSON.parse(body);
+      res.render("champion", { data });
+    } else {
+      console.log(error);
+    }
+  });
+});
+
+const port = Number(process.env.PORT || 3000);
 app.listen(port);

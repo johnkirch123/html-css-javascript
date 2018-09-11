f10342f8f55da6756f471698a69ac9c3f958b829
John Kirch <john.kirch.business@gmail.com>
2018-09-10 22:04:58 -0600

    express with routes

--- udemy/MERN/devconnector/routes/api/posts.js
+++ udemy/MERN/devconnector/routes/api/posts.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-// @route   GET api/users/test
-// @desc    Tests users route
+// @route   GET api/posts/test
+// @desc    Tests post route
 // @access  Public
 router.get("/test", (req, res) => {
-  res.json({ msg: "Users works" });
+  res.json({ msg: "Posts works" });
 });
 
 module.exports = router;

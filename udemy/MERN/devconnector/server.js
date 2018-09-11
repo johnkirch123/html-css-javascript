444ee850e76559d73b46a16403e2a5aad4337e88
John Kirch <john.kirch.business@gmail.com>
2018-09-10 21:03:53 -0600

    file structure created

--- udemy/MERN/devconnector/server.js
+++ udemy/MERN/devconnector/server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello"));
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB - promise
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to DB"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World"));
 
 const port = process.env.PORT || 5000;
 

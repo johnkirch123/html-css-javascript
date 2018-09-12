1fb34bf1c8367ca2b6019f95b7771398b0cb6081
johnkirch123 <john.kirch.business@gmail.com>
2018-09-12 12:13:29 -0600

    working on model viewer for 3d project

--- personal-projects/element-earth-app/src/components/modelViewer.jsx
+++ personal-projects/element-earth-app/src/components/modelViewer.jsx
@@ -7,17 +7,13 @@ class ModelViewer extends Component {
   state = {};
 
   componentDidMount() {
-    // this.THREE = THREE;
-    // const objLoader = new this.THREE.OBJLoader();
-    // objLoader.load("../assets/Environment_GroupTrees.obj", object => {});
-    // const viewerElement = document.getElementById("viewer");
-    // const scene = modelPlayer.prepareScene(viewerElement);
-    // modelPlayer.loadObject(scene, "./assets/Enviroment_GroupTrees.obj");
+    
   }
 
   render() {
-    this.THREE = THREE;
-    const objLoader = new this.THREE.OBJLoader();
+    return (
+
+    );
   }
 }
 

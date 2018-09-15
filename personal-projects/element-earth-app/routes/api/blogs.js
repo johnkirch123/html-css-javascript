e8e72a69ec2bce90c44d1eea25b5566cac0a21b0
johnkirch123 <john.kirch.business@gmail.com>
2018-09-15 01:07:01 -0600

    basic routes with express server - no keys

---
+++ personal-projects/element-earth-app/routes/api/blogs.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+// 2. Don't need api/users because it is 'used' by app.use() and prefixed
+// @route     GET api/blogs/test
+// @desc      Tests blogs route
+// @access    Public
+router.get("/test", (req, res) => {
+  res.json({ msg: "Blogs Works" });
+});
+
+module.exports = router;

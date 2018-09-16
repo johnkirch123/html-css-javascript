90284a32eb32ecf4c61a6ed4c4d7566f99c831d7
John Kirch <john.kirch.business@gmail.com>
2018-09-16 15:25:37 -0600

    Basic setup with MERN & Sass

---
+++ personal-projects/john-kirch/client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./css/style.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>John Kirch HomePage</h1>
+      </div>
+    );
+  }
+}
+
+export default App;

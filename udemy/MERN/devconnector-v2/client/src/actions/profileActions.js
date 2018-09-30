209074e75581f9b71928993e0f22fb648946e25a
John Kirch <john.kirch.business@gmail.com>
2018-09-29 22:50:33 -0600

    completed education and show developers.

--- udemy/MERN/devconnector-v2/client/src/actions/profileActions.js
+++ udemy/MERN/devconnector-v2/client/src/actions/profileActions.js
@@ -4,11 +4,12 @@ import {
   GET_PROFILE,
   PROFILE_LOADING,
   CLEAR_CURRENT_PROFILE,
+  GET_PROFILES,
   GET_ERRORS,
   SET_CURRENT_USER
 } from "./types";
 
-// // Get current profile
+// Get current profile
 export const getCurrentProfile = () => dispatch => {
   dispatch(setProfileLoading());
   axios
@@ -75,6 +76,43 @@ export const deleteExperience = id => dispatch => {
       dispatch({
         type: GET_ERRORS,
         payload: err.response.data
+      })
+    );
+};
+
+// Delete education
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Get all profiles
+export const getProfiles = () => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get("/api/profile/all")
+    .then(res =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILES,
+        payload: null
       })
     );
 };

90899b137181d1ede522b9f9aa46aeb1088fcf37
John Kirch <john.kirch.business@gmail.com>
2018-09-30 00:03:52 -0600

    completed setting up profile layout, basic

---
+++ udemy/MERN/devconnector-v2/client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ProfileAbout extends Component {
+  render() {
+    return <h1>TODO: Profile About</h1>;
+  }
+}
+
+export default ProfileAbout;
---
+++ udemy/MERN/devconnector-v2/client/src/components/profile/ProfileCreds.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class ProfileCreds extends Component {
+  render() {
+    return <h1>TODO: Profile Credentials</h1>;
+  }
+}
+
+export default ProfileCreds;

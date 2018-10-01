c895b65f35270c8060d5955ceacad81e9b0280a8
John Kirch <john.kirch.business@gmail.com>
2018-09-30 23:20:34 -0600

    Prepared for deploy

--- udemy/MERN/devconnector-v2/client/src/actions/postActions.js
+++ udemy/MERN/devconnector-v2/client/src/actions/postActions.js
@@ -3,13 +3,16 @@ import axios from "axios";
 import {
   ADD_POST,
   GET_POSTS,
+  GET_POST,
   DELETE_POST,
   GET_ERRORS,
-  POST_LOADING
+  POST_LOADING,
+  CLEAR_ERRORS
 } from "./types";
 
 // Add post
 export const addPost = postData => dispatch => {
+  dispatch(clearErrors());
   axios
     .post("/api/posts", postData)
     .then(res =>
@@ -41,6 +44,26 @@ export const getPosts = () => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_POSTS,
+        payload: null
+      })
+    );
+};
+
+// Get post
+export const getPost = id => dispatch => {
+  dispatch(setPostLoading());
+
+  axios
+    .get(`/api/posts/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_POST,
         payload: null
       })
     );
@@ -90,9 +113,53 @@ export const removeLike = id => dispatch => {
     );
 };
 
+// Add comment
+export const addComment = (postId, commentData) => dispatch => {
+  dispatch(clearErrors());
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete comment
+export const deleteComment = (postId, commentId) => dispatch => {
+  axios
+    .delete(`/api/posts/comment/${postId}/${commentId}`)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return {
     type: POST_LOADING
+  };
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS
   };
 };

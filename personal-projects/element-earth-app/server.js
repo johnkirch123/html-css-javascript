bc7a614812bc929938f0e20cdc4842eaabded060
johnkirch123 <john.kirch.business@gmail.com>
2018-10-04 20:03:17 -0600

    completed 6 Install dependencies and basic server setup

--- personal-projects/element-earth-app/server.js
+++ personal-projects/element-earth-app/server.js
@@ -1,3 +1,13 @@
+// 1. express
 const express = require("express"),
   mongoose = require("mongoose"),
   bodyParser = require("body-parser");
+
+// 1. initialize app
+const app = express();
+// 1. Test route
+app.get("/", (req, res) => res.send("Hello"));
+
+// 1. Create Port var then Run Server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}`));

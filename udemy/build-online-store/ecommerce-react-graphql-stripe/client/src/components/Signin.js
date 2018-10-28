81955181ba821b5a82d137505a51df005070f940
John Kirch <john.kirch.business@gmail.com>
2018-10-28 16:34:54 -0600

    changes to tutorial

--- udemy/build-online-store/ecommerce-react-graphql-stripe/client/src/components/Signin.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Signin extends Component {
-  render() {
-    return <div>Signin</div>;
-  }
-}
-
-export default Signin;
--- udemy/build-online-store/ecommerce-react-graphql-stripe/client/src/components/Signup.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class Signup extends Component {
-  render() {
-    return <div>Sign up - sign up here</div>;
-  }
-}
-
-export default Signup;

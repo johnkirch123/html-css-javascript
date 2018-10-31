e58e6c16130e6fbcaeee6029587eeddabbbe851a
John Kirch <john.kirch.business@gmail.com>
2018-10-31 00:02:55 -0600

    completed signin and navbar

--- udemy/build-online-store/ecommerce-react-graphql-stripe-1/client/src/components/Navbar.js
+++ udemy/build-online-store/ecommerce-react-graphql-stripe-1/client/src/components/Navbar.js
@@ -1,8 +1,73 @@
 import React from "react";
-import { Box, Text, Heading, Image } from "gestalt";
-import { NavLink } from "react-router-dom";
+import { Box, Text, Heading, Image, Button } from "gestalt";
+import { getToken, clearToken, clearCart } from "../utils/index";
+import { NavLink, withRouter } from "react-router-dom";
 
-const Navbar = () => (
+class Navbar extends React.Component {
+  handleSignout = () => {
+    // clear token
+    clearToken();
+    // clear cart
+    clearCart();
+    // redirect home
+    this.props.history.push("/");
+  };
+
+  render() {
+    return getToken() !== null ? (
+      <AuthNav handleSignout={this.handleSignout} />
+    ) : (
+      <UnAuthNav />
+    );
+  }
+}
+
+const AuthNav = ({ handleSignout }) => (
+  <Box
+    display="flex"
+    alignItems="center"
+    justifyContent="around"
+    height={70}
+    color="midnight"
+    padding={1}
+    shape="roundedBottom"
+  >
+    {/* Checkout Link */}
+    <NavLink activeClassName="active" to="/checkout">
+      <Text size="xl" color="white">
+        Checkout
+      </Text>
+    </NavLink>
+
+    {/* Title and Logo */}
+    <NavLink activeClassName="active" to="/">
+      <Box display="flex" alignItems="center">
+        <Box margin={2} height={50} width={50}>
+          <Image
+            alt="BrewHaha Logo"
+            naturalHeight={1}
+            naturalWidth={1}
+            src="./icons/logo.svg"
+          />
+        </Box>
+        <Heading size="xs" color="orange">
+          BrewHaha
+        </Heading>
+      </Box>
+    </NavLink>
+
+    {/* Sign out Link */}
+    <Button
+      onClick={handleSignout}
+      inline
+      color="transparent"
+      text="Sign Out"
+      size="md"
+    />
+  </Box>
+);
+
+const UnAuthNav = () => (
   <Box
     display="flex"
     alignItems="center"
@@ -45,4 +110,4 @@ const Navbar = () => (
   </Box>
 );
 
-export default Navbar;
+export default withRouter(Navbar);

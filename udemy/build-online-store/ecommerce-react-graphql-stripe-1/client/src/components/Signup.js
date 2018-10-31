4dfa7ec7bdfd02b931ae5dd93689b8b65dc2ea5a
John Kirch <john.kirch.business@gmail.com>
2018-10-30 23:20:23 -0600

    completed jwt authentication

--- udemy/build-online-store/ecommerce-react-graphql-stripe-1/client/src/components/Signup.js
+++ udemy/build-online-store/ecommerce-react-graphql-stripe-1/client/src/components/Signup.js
@@ -1,8 +1,139 @@
 import React, { Component } from "react";
+import { Box, Container, Button, Heading, Text, TextField } from "gestalt";
+import ToastMessage from "./ToastMessage";
+import { setToken } from "../utils";
+import Strapi from "strapi-sdk-javascript/build/main";
+const apiUrl = process.env.API_URL || "http://localhost:1337";
+const strapi = new Strapi(apiUrl);
 
 class Signup extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    toast: false,
+    toastMessage: "",
+    loading: false
+  };
+  handleChange = ({ event, value }) => {
+    event.persist();
+    this.setState({ [event.target.name]: value });
+  };
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    const { username, email, password } = this.state;
+
+    if (this.isFormEmpty(this.state)) {
+      this.showToast("Fill in all Fields");
+      return;
+    }
+    // Sign up user
+    try {
+      // set loading - true
+      this.setState({ loading: true });
+      // make request to register user with strapi
+      const response = await strapi.register(username, email, password);
+      // set loading false
+      this.setState({ loading: false });
+      // put token (to manage user session) in local storage
+      setToken(response.jwt);
+      // console.log(response);
+      this.redirectUser("/");
+      // redirect user to home page
+    } catch (err) {
+      // set loading - false
+      this.setState({ loading: false });
+      // show error message with toast message
+      this.showToast(err.message);
+    }
+  };
+
+  redirectUser = path => this.props.history.push(path);
+
+  isFormEmpty = ({ username, email, password }) => {
+    return !username || !email || !password;
+  };
+
+  showToast = toastMessage => {
+    this.setState({ toast: true, toastMessage });
+    setTimeout(() => this.setState({ toast: false, toastMessage: "" }), 5000);
+  };
+
   render() {
-    return <div>Sign up - sign up here</div>;
+    const { toastMessage, toast, loading } = this.state;
+
+    return (
+      <Container>
+        <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: "#ebe2da"
+            }
+          }}
+          margin={4}
+          padding={4}
+          shape="rounded"
+          display="flex"
+          justifyContent="center"
+        >
+          {/* Sign Up Form */}
+          <form
+            style={{
+              display: "inlineBlock",
+              textAlign: "center",
+              maxWidth: 450
+            }}
+            onSubmit={this.handleSubmit}
+          >
+            {/* Sign Up Form Heading */}
+            <Box
+              marginBottom={2}
+              display="flex"
+              direction="column"
+              alignItems="center"
+            >
+              <Heading color="midnight">Let's Get Started</Heading>
+              <Text italic color="orchid">
+                Sign up to order some brews!
+              </Text>
+            </Box>
+            {/* Username Input */}
+            <TextField
+              id="username"
+              type="text"
+              name="username"
+              placeholder="Username"
+              onChange={this.handleChange}
+            />
+            {/* Email Input */}
+            <TextField
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={this.handleChange}
+            />
+            {/* Password Input */}
+            <TextField
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.handleChange}
+            />
+            <Button
+              inline
+              disabled={loading}
+              color="blue"
+              text="Submit"
+              type="submit"
+            />
+          </form>
+        </Box>
+        <ToastMessage show={toast} message={toastMessage} />
+      </Container>
+    );
   }
 }
 

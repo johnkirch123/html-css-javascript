070cb9f8fbb2cf4448c47e27f8279678de9df463
John Kirch <john.kirch.business@gmail.com>
2018-11-15 05:14:10 -0700

    change to reducer, need to hook up api to react products page

--- personal-projects/element-earth-app-redux/client/src/reducers/productsReducer.js
+++ personal-projects/element-earth-app-redux/client/src/reducers/productsReducer.js
@@ -1,4 +1,9 @@
 export default () => {
   // query product endpoint to fill store
-  return { name: "Product 1", description: "Great product" };
+  return [
+    {name: "Product 1", description: "Great product"},
+    {name: "Product 2", description: "Great product"},
+    {name: "Product 3", description: "Great product"}
+    
+  ];
 };

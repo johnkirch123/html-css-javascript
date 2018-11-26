8e27054cf8b57de118addcfeb725d4ae10bc32ce
johnkirch123 <john.kirch.business@gmail.com>
2018-11-25 21:05:21 -0700

    working on login

--- personal-projects/element-earth-app-redux/client/src/components/Signin.jsx
+++ personal-projects/element-earth-app-redux/client/src/components/Signin.jsx
@@ -1,15 +1,59 @@
 import React, { Component } from "react";
 
 class Signin extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: "",
+      password: "",
+      errors: {}
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
   render() {
     const { routeHandler } = this.props;
     return (
-      <div>
+      <section className="login u-container u-center-text">
         {this.props.route !== this.props.match.path
           ? routeHandler(this.props.match.path)
           : ""}
-        Signin
-      </div>
+        <div className="login__form">
+          <form action="#" className="form">
+            <h1 className="form__heading">Login</h1>
+            <div className="form__group">
+              <input
+                type="email"
+                className="form__input"
+                placeholder="Email"
+                id="email"
+                required
+              />
+              <label for="email" className="form__label">
+                Email
+              </label>
+            </div>
+            <div className="form__group">
+              <input
+                type="password"
+                className="form__input"
+                placeholder="Password"
+                id="password"
+                required
+              />
+              <label for="password" className="form__label">
+                Password
+              </label>
+            </div>
+            <div className="form__group">
+              <button className="btn btn--green">Login</button>
+            </div>
+          </form>
+        </div>
+      </section>
     );
   }
 }

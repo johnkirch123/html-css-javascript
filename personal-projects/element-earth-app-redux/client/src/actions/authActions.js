877e145737f08bf756cf1bc616a306146dc7c3c1
johnkirch123 <john.kirch.business@gmail.com>
2018-11-27 09:58:07 -0700

    completed errors and axios login

--- personal-projects/element-earth-app-redux/client/src/actions/authActions.js
+++ personal-projects/element-earth-app-redux/client/src/actions/authActions.js
@@ -12,3 +12,15 @@ export const registerUser = userData => dispatch => {
       })
     );
 };
+
+export const loginUser = userData => dispatch => {
+  axios
+    .post("api/users/login", userData)
+    .then(res => console.log(res.data))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
--- personal-projects/element-earth-app-redux/client/src/components/Login.jsx
+++ personal-projects/element-earth-app-redux/client/src/components/Login.jsx
@@ -1,4 +1,9 @@
 import React, { Component } from "react";
+import axios from "axios";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { loginUser } from "../actions/authActions";
 
 class Login extends Component {
   constructor() {
@@ -14,6 +19,12 @@ class Login extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   onChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
@@ -21,15 +32,32 @@ class Login extends Component {
   onSubmit(e) {
     e.preventDefault();
 
-    const user = {
+    const newUser = {
       email: this.state.email,
       password: this.state.password
     };
-    console.log(user);
+    // console.log(newUser);
+    axios
+      .post("/api/users/login", newUser)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
+    // this.props.registerUser(newUser);
   }
+
+  // onSubmit(e) {
+  //   e.preventDefault();
+
+  //   const user = {
+  //     email: this.state.email,
+  //     password: this.state.password
+  //   };
+  //   console.log(user);
+  // }
 
   render() {
     const { routeHandler } = this.props;
+    const { errors } = this.state;
+
     return (
       <section className="login u-container u-center-text">
         {this.props.route !== this.props.match.path
@@ -42,25 +70,33 @@ class Login extends Component {
               <input
                 type="email"
                 name="email"
-                className="form__input"
+                className={classnames("form__input", {
+                  error: errors.email
+                })}
                 placeholder="Email"
                 id="email"
                 value={this.state.email}
                 onChange={this.onChange}
               />
               <label className="form__label">Email</label>
+              {errors.password && <div className="errors">{errors.email}</div>}
             </div>
             <div className="form__group">
               <input
                 type="password"
                 name="password"
-                className="form__input"
+                className={classnames("form__input", {
+                  error: errors.password
+                })}
                 placeholder="Password"
                 id="password"
                 value={this.state.password}
                 onChange={this.onChange}
               />
               <label className="form__label">Password</label>
+              {errors.password && (
+                <div className="errors">{errors.password}</div>
+              )}
             </div>
             <div className="form__group">
               <button className="btn btn--green">Login</button>
@@ -72,4 +108,18 @@ class Login extends Component {
   }
 }
 
-export default Login;
+Login.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { loginUser }
+)(Login);

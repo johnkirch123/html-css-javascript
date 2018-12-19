3bb3f3e934a2a10f131633dd91b813c0118e2001
johnkirch123 <john.kirch.business@gmail.com>
2018-12-19 11:43:32 -0700

    completed uploading a file

--- traversy-media/uploading-files-mongodb/app.js
+++ traversy-media/uploading-files-mongodb/app.js
@@ -1,14 +1,63 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 const crypto = require("crypto");
 const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const methodOverride = require("method-override");
+const MONGO_URI = require("./keys/keys").mongoURI;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
+// Create mongo connection
+const conn = mongoose.createConnection(MONGO_URI);
+
+// Init gfs
+let gfs;
+
+conn.once("open", () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("uploads");
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: MONGO_URI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads"
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
+// @route GET /
+// @desc Loads Form
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+// @route POST /upload
+// @desc Uploads file to DB
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.json({ file: req.file });
 });
 
 const port = 5000;

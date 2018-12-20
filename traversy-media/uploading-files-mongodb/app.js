b2943894c50553b62fdf95b24aa11a717b98af0a
johnkirch123 <john.kirch.business@gmail.com>
2018-12-20 09:48:44 -0700

    not deleting images uploading files to mongo deprecationWarning

--- traversy-media/uploading-files-mongodb/app.js
+++ traversy-media/uploading-files-mongodb/app.js
@@ -51,13 +51,99 @@ const upload = multer({ storage });
 // @route GET /
 // @desc Loads Form
 app.get("/", (req, res) => {
-  res.render("index");
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render("index", { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === "image/jpeg" ||
+          file.contentType === "image/png"
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+    }
+
+    // Files exist
+    res.render("index", { files });
+  });
 });
 
 // @route POST /upload
 // @desc Uploads file to DB
 app.post("/upload", upload.single("file"), (req, res) => {
-  res.json({ file: req.file });
+  // res.json({ file: req.file });
+  res.redirect("/");
+});
+
+// @route GET /files
+// @desc Display all files in JSON
+app.get("/files", (req, res) => {
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: "No files exist"
+      });
+    }
+
+    // Files exist
+    return res.json(files);
+  });
+});
+
+// @route GET /files/:filename
+// @desc Display single file in JSON
+app.get("/files/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if files
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exists"
+      });
+    }
+
+    // File exists
+    return res.json(file);
+  });
+});
+
+// @route GET /image/:filename
+// @desc Display image
+app.get("/image/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if files
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exists"
+      });
+    }
+
+    // Check if image
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image"
+      });
+    }
+  });
+});
+
+// @route DELETE /files/:id
+// @desc Delete file
+app.delete("/files/:id", (req, res) => {
+  gfs.remove({ _id: req.params.id, root: "uploads" }, (err, gridStore) => {
+    if (err) return res.status(404).json({ err });
+    console.log("Image removed");
+    res.redirect("/");
+  });
 });
 
 const port = 5000;

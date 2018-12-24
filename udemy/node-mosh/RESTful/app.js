3b1006f12529ad76c0bb1258e3d3e179eb4732e1
johnkirch123 <john.kirch.business@gmail.com>
2018-12-24 03:47:12 -0700

    using get requests

--- udemy/node-mosh/RESTful/app.js
+++ udemy/node-mosh/RESTful/app.js
@@ -1,17 +1,46 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello World");
-    res.end();
-  }
+const app = express();
 
-  if (req.url === "/api/customers") {
-    res.write(JSON.stringify([1, 2, 3]));
-    res.end();
-  }
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" }
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello World!!!");
 });
 
-server.listen(3001);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-console.log("Listening on port 3001...");
+app.get("/api/courses/:id", (req, res) => {
+  // res.send(req.params.id);
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).json({ err: "Course does not exist" });
+  res.send(course);
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => console.log("Server running on port: " + port));
+
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.write("Hello World");
+//     res.end();
+//   }
+
+//   if (req.url === "/api/customers") {
+//     res.write(JSON.stringify([1, 2, 3]));
+//     res.end();
+//   }
+// });
+
+// server.listen(3001);
+
+// console.log("Listening on port 3001...");

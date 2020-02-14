eb0cd22586a7384f5b44061cfd9776e13ccbf4e4
John Kirch <john.kirch.business@gmail.com>
2020-02-13 20:46:05 -0700

    two way binding

--- udemy/angular/my-first-app/src/app/app.module.ts
+++ udemy/angular/my-first-app/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { MessageComponent } from './messages/messages.component';
+import { TwowaybindingComponent } from './twowaybinding/twowaybinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    MessageComponent
+    MessageComponent,
+    TwowaybindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- udemy/angular/my-first-app/src/app/servers/servers.component.ts
+++ udemy/angular/my-first-app/src/app/servers/servers.component.ts
@@ -7,14 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
+  serverName: string = 'TestServer';
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    }, 2000);
+    }, 1000);
    }
 
   ngOnInit(): void {
   }
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }

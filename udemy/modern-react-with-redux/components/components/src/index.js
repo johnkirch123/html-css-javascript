1996db19111971b477caaee3bbd69e5e9fcb9eef
John Kirch <john.kirch.business@gmail.com>
2020-06-10 17:43:37 -0600

    communicating with props

--- udemy/modern-react-with-redux/components/components/src/index.js
+++ udemy/modern-react-with-redux/components/components/src/index.js
@@ -1,14 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-import Comment from './CommentDetail';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <Comment />
-      <Comment />
-      <Comment />
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          commentDate="Today at 5:00PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          commentDate="Today at 5:15PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author={faker.name.firstName()}
+          commentDate="Today at 5:16PM"
+          content={faker.lorem.sentence()}
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };

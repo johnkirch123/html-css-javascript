222335b034c1bc2edfd58d03cc21c8bc2f80a19d
John Kirch <john.kirch.business@gmail.com>
2020-06-15 18:54:56 -0600

    created spinner component and refactored

--- udemy/modern-react-with-redux/class-based-components/src/index.js
+++ udemy/modern-react-with-redux/class-based-components/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 // const App = () => {
 //   window.navigator.geolocation.getCurrentPosition(
@@ -20,7 +21,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -29,7 +30,11 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <div>Loading...</div>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div className="border-red">{this.renderContent()}</div>;
   }
 }
 

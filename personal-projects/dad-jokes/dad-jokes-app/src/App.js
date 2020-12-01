f6c20b6f213a4c9426205e2319171fef51d6f4d3
John Kirch <dubskigaming@hotmail.com>
2020-12-01 14:28:35 -0700

    complete dad joke app

--- personal-projects/dad-jokes/dad-jokes-app/src/App.js
+++ personal-projects/dad-jokes/dad-jokes-app/src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Joke from './components/Joke';
+import About from './components/pages/About'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="container">
-        <Joke />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="container mx-auto">
+          <Route path="/" exact component={Joke} />
+          <Route path="/about" exact component={About} />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
